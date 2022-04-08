20c72561385911481827fbd027939f240506ce4e
weiZhen <likelovec@gmail.com>
2022-04-08 15:57:18 +0800

    add more ws endpoint register

--- src/main/java/io/github/fzdwx/inf/middleware/sky/Ws.java
+++ src/main/java/io/github/fzdwx/inf/middleware/sky/Ws.java
@@ -9,8 +9,39 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * mark is websocket handler.
+ * <p>
+ * just mark.
+ *
  * @author <a href="mailto:likelovec@gmail.com">fzdwx</a>
  * @date 2022/4/4 18:37
+ * @apiNote <pre>{@code @Ws("/hello")
+ * public class HelloWs implements HttpHandler {
+ *      public void handle(final HttpServerRequest request, final HttpServerResponse resp) throws Exception {
+ *          request.upgradeToWebSocket()
+ *           .then(ws->{
+ *
+ *               // your code.
+ *
+ *           })
+ *      }
+ * }
+ *
+ * @Ws("/hello2")
+ * public class HelloWs2 {
+ *      // method name must is handle, and have httpServerRequest and httpServerResponse parameter.
+ *      public void handle(final HttpServerRequest request, final HttpServerResponse resp) throws Exception {
+ *          request.upgradeToWebSocket()
+ *           .then(ws->{
+ *
+ *               // your code.
+ *
+ *           })
+ *      }
+ * }}
+ * </pre>
+ * @see io.github.fzdwx.inf.http.core.HttpHandler
+ * @see io.github.fzdwx.inf.middleware.sky.config.WsConfiguration
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
@@ -18,6 +49,9 @@ import java.lang.annotation.Target;
 @Component
 public @interface Ws {
 
+    /**
+     * path
+     */
     String value();
 
 }
--- src/main/java/io/github/fzdwx/logic/msg/ws/endpoint/EchoWs.java
+++ src/main/java/io/github/fzdwx/logic/msg/ws/endpoint/EchoWs.java
@@ -2,7 +2,6 @@ package io.github.fzdwx.logic.msg.ws.endpoint;
 
 import cn.dev33.satoken.stp.StpUtil;
 import io.github.fzdwx.inf.common.web.Web;
-import io.github.fzdwx.inf.http.core.HttpHandler;
 import io.github.fzdwx.inf.http.core.HttpServerRequest;
 import io.github.fzdwx.inf.http.core.HttpServerResponse;
 import io.github.fzdwx.inf.middleware.sky.Ws;
@@ -16,9 +15,8 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Ws("/echo")
 @Slf4j
-public class EchoWs implements HttpHandler {
+public class EchoWs {
 
-    @Override
     public void handle(final HttpServerRequest request, final HttpServerResponse resp) throws Exception {
         final var token = request.params().get(StpUtil.getTokenName());
         if (token == null) {
