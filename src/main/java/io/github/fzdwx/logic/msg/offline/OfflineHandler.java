d2f6c816c613ead48ffe37dc3b1025ea2de59ffa
weiZhen <likelovec@gmail.com>
2022-04-12 15:50:20 +0800

    temp

---
+++ src/main/java/io/github/fzdwx/logic/msg/offline/OfflineHandler.java
@@ -0,0 +1,17 @@
+package io.github.fzdwx.logic.msg.offline;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author <a href="mailto:likelovec@gmail.com">韦朕</a>
+ * @date 2022/4/12 15:48
+ */
+@Component
+public class OfflineHandler implements InitializingBean {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+}
