f6f178cef050abc369d5c4113451cbc370b88770
zhiyuan <1121317315@qq.com>
2022-04-27 11:45:24 +0800

    code clean

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,6 @@
 Path=./chat-0.01.jar
 ACTION=$1
 JAVA_OUT=./start.log  #应用的启动日志
-Config=./application-prod.yml #配置文件位置
 
 
 usage() {
@@ -21,7 +20,7 @@ usage() {
 start_application() {
     echo "starting java process"
     echo "${Path}"
-    nohup java -javaagent:/root/project/xrebel.jar -jar ${Path} -Dspring.config.location=${Config} > ${JAVA_OUT} 2>&1 &
+    nohup java -javaagent:/root/project/xrebel.jar -jar ${Path} > ${JAVA_OUT} 2>&1 &
     echo "started java process"
 }
 
