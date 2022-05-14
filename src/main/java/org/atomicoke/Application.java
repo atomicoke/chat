7e0aa9db72e28e877f55c5d41cc31be1e2653bd0
fzdwx <likelovec@gmail.com>
2022-05-14 19:54:02 +0800

    add banner print

--- src/main/java/org/atomicoke/Application.java
+++ src/main/java/org/atomicoke/Application.java
@@ -1,5 +1,7 @@
 package org.atomicoke;
 
+import io.github.fzdwx.lambada.Console;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,11 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author <a href="mailto:likelovec@gmail.com">fzdwx</a>
  * @date 2022/4/3 16:06
  */
+@Slf4j
 @SpringBootApplication
 @MapperScan("org.atomicoke.logic.modules.*.domain.dao.mapper")
 public class Application {
 
     public static void main(String[] args) throws InterruptedException {
+        log.info(Console.banner());
         final var context = SpringApplication.run(Application.class);
     }
 }
